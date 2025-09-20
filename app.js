1b3114ef8a249957c67b1acc83085190dd38b1a3
arc-1409 <lulu.august.liao@gmail.com>
2025-09-20 12:38:56 -0400

    add getPositionPrem

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const puppeteer = require("puppeteer");
 
-async function webScrapPrem() {
+async function getPositionPrem(teamName, tagged) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto("https://www.bbc.com/sport/football/premier-league/table");
+    await page.goto("https://www.bbc.com/sport/football/premier-league/table", { waitUntil: "networkidle2"});
+    await page.waitForSelector("table tbody tr");
+
+    const teamsList = await page.$$eval("table tbody tr", rows => {
+        return rows.map(row => {
+            const position = row.querySelector("td:first-child")?.textContent.trim();
+            const name = row.querySelector("td:team")?.textContent.trim();
+            return { position, name };
+        });
+    });
+
+    const team = teamsList.find(t => t.name?.toLowerCases() === teamName.toLowerCase());
+
+    if(tagged) {
+        if(team) {
+            console.log("${team.name} is currently in position ${team.position} on the premier league.");
+        } else {
+            console.lot("${team.name} is not on Premier League.");
+        }
+    } else {
+        if(team) {
+            console.log("${team.name} is currently in position ${team.position} on the premier league.");
+        }
+    }
 }
