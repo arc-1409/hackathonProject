233408e40b03a427505f83118b7a07e8cb68be6d
Edward Ju <ju51@purdue.edu>
2025-09-20 14:05:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,62 +66,6 @@ program
 
     program.parse(process.argv);
         
-    
-    
-    function findTeam(input, teams) {
-      const code = input.toUpperCase();
-      if (teams[code]) return teams[code];
-      const match = Object.values(teams).find(
-        (t) => t.toLowerCase() === input.toLowerCase()
-      );
-      return match || input;
-    }
-    
-    const { getPositionPrem, getPositionLaLiga, getFullPrem, getFullLaLiga } = require("./app");
-    
-    async function askLeague() {
-      const { league } = await inquirer.prompt([
-        {
-          type: "list",
-          name: "league",
-          message: "Which league are you finding?",
-          choices: ["Premier League", "La Liga"]
-        }
-      ]);
-    
-      const { specific } = await inquirer.prompt([
-        {
-          type: "confirm",
-          name: "specific",
-          message: "Are you looking for a specific team?",
-          default: true
-        }
-      ]);
-    
-      if (specific) {
-        const { team } = await inquirer.prompt([
-          {
-            type: "input",
-            name: "team",
-            message: "Enter the team name:"
-          }
-        ]);
-    
-        if (league === "Premier League") {
-          await getPositionPrem(team, true);
-        } else {
-          await getPositionLaLiga(team, true);
-        }
-      } else {
-        if (league === "Premier League") {
-          await getFullPrem();
-        } else {
-          await getFullLaLiga();
-        }
-      }
-    }
-    
-    askLeague();
 
 
 
