a249a37204ff24a2e7371c514f31e2cd12dfe1e4
arc-1409 <lulu.august.liao@gmail.com>
2025-09-21 11:20:00 -0400

    table html & modifications

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ import puppeteer from "puppeteer";
 
 async function getPositionPrem(page, teamName) {
     await page.goto("https://www.bbc.com/sport/football/premier-league/table", { waitUntil: "networkidle2"});
-    await page.waitForSelector("table tbody tr");
+    await page.waitForSelector("ssrcss-1urqilq-CellsRow.e13j9mpy2");
 
-    const teamsList = await page.$$eval("table tbody tr", rows => {
+    const teamsList = await page.$$eval("tr.ssrcss-1urqilq-CellsRow.e13j9mpy2", rows => {
         return rows.map(row => {
-            const position = row.querySelector("td:first-child")?.textContent.trim();
-            const name = row.querySelector("td.team")?.textContent.trim();
-            return { position, name };
+            const rank = row.querySelector("span.ssrcss-4fgj5b-Rank")?.textContent.trim();
+            const name = row.querySelector("span.visually-hidden")?.textContent.trim();
+            return { rank, name };
         });
     });
 
     const team = teamsList.find(t => t.name?.toLowerCase() === teamName.toLowerCase());
 
     if(team) {
-        console.log(`${teamName} is currently in position ${team.position} on the premier league.`);
+        console.log(`${teamName} is currently in position ${team.rank} on the premier league.`);
     } 
     else {
         console.log(`${teamName} is not on Premier League.`);
@@ -28,16 +28,16 @@ async function getPositionLaLiga(page, teamName) {
 
     const teamsList = await page.$$eval("table tbody tr", rows => {
         return rows.map(row => {
-            const position = row.querySelector("td:first-child")?.textContent.trim();
+            const rank = row.querySelector("td:first-child")?.textContent.trim();
             const name = row.querySelector("td.team")?.textContent.trim();
-            return { position, name };
+            return { rank, name };
         });
     });
 
     const team = teamsList.find(t => t.name?.toLowerCase() === teamName.toLowerCase());
 
     if(team) {
-        console.log(`${teamName} is currently in position ${team.position} in la liga.`); } 
+        console.log(`${teamName} is currently in position ${team.rank} in la liga.`); } 
     else {
         console.log(`${teamName} is not in la liga.`); } 
 }
