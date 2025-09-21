4b33a50820b6a9b3e89633290311f83ff61638c3
arc-1409 <lulu.august.liao@gmail.com>
2025-09-21 03:11:36 -0400

    require -> import puppeteer

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 
 async function getPositionPrem(page, teamName) {
     await page.goto("https://www.bbc.com/sport/football/premier-league/table", { waitUntil: "networkidle2"});
