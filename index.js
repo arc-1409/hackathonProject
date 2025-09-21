2251e795afb6e9a1d6bcc80e5deb52060fcc710a
arc-1409 <lulu.august.liao@gmail.com>
2025-09-21 00:40:21 -0400

    cleaner code & help page

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const teamList = {
 
 if (options.help) {
     console.log(`
-=== FootballWatcher User Guide ===
+================================ FootballWatcher User Guide ================================
 
 Commands: 
     -h, --help              User guide
@@ -103,62 +103,3 @@ else if (leagueName === "La Liga") {
 else {
     getPositionPrem(teamName);
     getPositionLaLiga(teamName); }
-
-
-
-
-
-
-
-/*
-console.log(
-    chalk.magenta(figlet.textSync("CLI DEMO", { horizontalLayout:"full" }))
-);
-
-
-const promptMode = [
-    {
-        type: "list",
-        name: "mode prompt",
-        message: "seek it out and ye shall find.",
-        choices: [
-            "league table lookup",
-            "team position lookup",
-            "future match lookup"
-        ]
-    }
-];
-
-const promptLeague = [
-    {
-        type: "list",
-        name: "league prompt",
-        message: "pick a league: ",
-        choices: [
-            "premier league",
-            "la liga",
-            "uefa champions leauge"
-        ]
-    }
-];
-
-const promptTeam = [
-    {
-        type: "input",
-        name: "team prompt",
-        message: "enter any team on the premier league, laliga, or champions league: ",
-        validate(team) {
-            if(!team) {
-                return "Enter a team name."
-            }
-            if(team.notOntTable()) {
-                return "Enter a team that's actually on the table."
-            }
-            return true;
-        }
-    }
-];
-*/
-
-
-
