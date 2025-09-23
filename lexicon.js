2e356ffa2943b5ab7567946f688f84e377282cde
arc-1409 <lulu.august.liao@gmail.com>
2025-09-23 15:03:18 -0400

    move lists and export

--- lexicon.js
+++ lexicon.js
@@ -1,1 +1,69 @@
+const leagueList = {
+    prem: "Premier League",
+    laliga: "La Liga"
+};
 
+const teamList = {
+    ars: "Arsenal",
+    avl: "Aston Villa", 
+    bou: "AFC Bournemouth",
+    bre: "Brentford",
+    bha: "Brighton & Hove Albion",
+    che: "Chelsea",
+    cry: "Crystal Palace",
+    eve: "Everton",
+    ful: "Fulham",
+    liv: "Liverpool",
+    lee: "Leeds United",
+    mci: "Manchester City",
+    mun: "Manchester United",
+    new: "Newcastle United",
+    nfo: "Nottingham Forest",
+    sou: "Southampton",
+    sun: "Sunderland",
+    tot: "Tottenham Hotspur",
+    whu: "West Ham United",
+    wol: "Wolverhampton Wanderers",
+    alv: "Alavés",
+    ath: "Athletic Club",
+    atm: "Atletico Madrid",
+    bar: "Barcelona",
+    bet: "Real Betis",
+    cel: "Celta Vigo", 
+    esp: "Espanyol",
+    elc: "Elche",
+    get: "Getafe",
+    gir: "Girona",
+    lev: "Levante",
+    mlc: "Mallorca",
+    osa: "Osasuna",
+    ovi: "Real Oviedo",
+    ray: "Rayo Vallecano",
+    rma: "Real Madrid",
+    rso: "Real Sociedad",
+    sev: "Sevilla",
+    val: "Valencia",
+    vil: "Villarreal"
+};
+
+const positionList = {
+    st: "striker",
+    rw: "right wing",
+    lw: "left wing",
+    cf: "centre forward",
+    rf: "right foward",
+    lf: "left forward",
+    cam: "central attacking midfielder",
+    cm: "central midfielder",
+    lm: "left midfielder",
+    rm: "right midfielder",
+    cdm: "central defensive midfielder",
+    cb: "centre back",
+    lwb: "left wing back",
+    rwb: "right wing back",
+    lb: "left back",
+    rb: "rb",
+    gk: "goalkeeper"
+};
+
+export { leagueList, teamList, positionList };
