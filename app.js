959f20c75658b87cf8dd104c476fb6130ddf827c
arc-1409 <lulu.august.liao@gmail.com>
2025-09-26 21:59:57 -0400

    try fixing bugs for laliga

--- app.js
+++ app.js
@@ -25,20 +25,24 @@ async function getPositionPrem(page, teamName) {
 
 async function getPositionLaLiga(page, teamName) {
     await page.goto("https://www.bbc.com/sport/football/spanish-la-liga/table", { waitUntil: "networkidle2"});
-    await page.waitForSelector("tr.ssrcss-lurqilq-CellsRow.e13j9mpy2");
+    await page.waitForSelector("tr.ssrcss-lurqilq-CellsRow");
 
-    const teamsList = await page.$$eval("tr.ssrcss-1urqilq-CellsRow.e13j9mpy2", rows => {
+    const teamsList = await page.$$eval("tr.ssrcss-1urqilq-CellsRow", rows => {
         return rows.map(row => {
-            const rank = row.querySelector("span.ssrcss-4fgj5b-Rank")?.textContent.trim();
-            const name = row.querySelector("a span.visually-hidden")?.textContent.trim();
+            const rank = row.querySelector("span.ssrcss-4fgj5b-Rank")?.innerText.trim();
+            
+            let name = row.querySelector("span[aria-hidden='true'][data-600]")?.getAttribute("data-600")?.trim();
+            if (!name) {
+                name = row.querySelector("span.visually-hidden")?.innerText.trim();
+            }
             return { rank, name };
         });
     });
 
-    const team = teamsList.find(t => t.name?.toLowerCase() === teamName.toLowerCase());
+    const teamTarget = teamsList.find(t => t.name?.toLowerCase() === teamName.toLowerCase());
 
-    if(team) {
-        console.log(`${teamName} is currently in position ${team.rank} in la liga.`); } 
+    if(teamTarget) {
+        console.log(`${teamName} is currently in position ${teamTarget.rank} in la liga.`); } 
     else {
         console.log(`${teamName} is not in la liga.`); } 
 }
