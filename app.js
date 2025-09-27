1ec5914889bfedb06c24f858cf3467bb24d1dfb2
arc-1409 <lulu.august.liao@gmail.com>
2025-09-27 01:09:57 -0400

    add getPositionBund

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 
+// english premier league
 async function getPositionPrem(page, teamName) {
     await page.goto("https://www.bbc.com/sport/football/premier-league/table", { waitUntil: "networkidle2"});
     await page.waitForSelector("tr[class*='CellsRow']");
@@ -28,7 +29,7 @@ async function getPositionPrem(page, teamName) {
     } // no need to close page; index.js does it already
 }
 
-
+// spanish la liga
 async function getPositionLaLiga(page, teamName) {
     await page.goto("https://www.bbc.com/sport/football/spanish-la-liga/table", { waitUntil: "networkidle2"});
     await page.waitForSelector("tr[class*='CellsRow']");
@@ -55,4 +56,31 @@ async function getPositionLaLiga(page, teamName) {
         console.log(`${teamName} is not in la liga.`); } 
 }
 
-export { getPositionPrem, getPositionLaLiga };
+// german bundesliga
+async function getPositionBund(page, teamName) {
+    await page.goto("https://www.bbc.com/sport/football/german-bundesliga/table", { waitUntil: "networkidle2"});
+    await page.waitForSelector("tr[class*='CellsRow']");
+
+    const teamsList = await page.$$eval("tr[class*='CellsRow']", rows => {
+        return rows.map(row => {
+            const rank = row.querySelector("span.ssrcss-4fgj5b-Rank")?.innerText.trim();
+
+            // Try aria-hidden first, fallback to visually-hidden
+            // fix 2: try visually-hidden, fallback to aria-hidden
+            let name = row.querySelector("span.visually-hidden")?.innerText.trim(); 
+            if (!name) {
+                name = row.querySelector("span[aria-hidden='true'][data-600]")?.getAttribute("data-600")?.trim();
+            }
+            return { rank, name };
+        });
+    });
+
+    const teamTarget = teamsList.find(t => t.name?.toLowerCase() === teamName.toLowerCase());
+
+    if(teamTarget) {
+        console.log(`${teamName} is currently in position ${teamTarget.rank} in german bundesliga.`); } 
+    else {
+        console.log(`${teamName} is not in german bundesliga.`); } 
+}
+
+export { getPositionPrem, getPositionLaLiga, getPositionBund };
