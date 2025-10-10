9f7595791e57cba24897a763e76ad76a1e30d4d9
arc-1409 <lulu.august.liao@gmail.com>
2025-10-10 12:18:34 -0400

    attempt 3: use boolean to check if scraped successfully, while loop

--- algorithms.js
+++ algorithms.js
@@ -11,6 +11,8 @@ async function searchStanding(page, obj) {
         console.error("ERROR: undefined teamName");
     }
 
+    let found = false;
+
     // filter leagues
     if(obj.league === "Premier League") {
         scrape("https://www.bbc.com/sport/football/premier-league/table");
@@ -19,14 +21,16 @@ async function searchStanding(page, obj) {
     } else if (obj.league === "German Bundesliga") {
         scrape("https://www.bbc.com/sport/football/german-bundesliga/table");
     } else {  // for when league isn't specified2
-        console.log("should have three undefined?");
-        scrape("https://www.bbc.com/sport/football/premier-league/table");
+        while (found === false) {
+            console.log("should have three undefined?");
+            scrape("https://www.bbc.com/sport/football/premier-league/table");
 
-        console.log("or is it just stuck here");
-        scrape("https://www.bbc.com/sport/football/spanish-la-liga/table");
+            console.log("or is it just stuck here");
+            scrape("https://www.bbc.com/sport/football/spanish-la-liga/table");
 
-        console.log("here maybe?");
-        scrape("https://www.bbc.com/sport/football/german-bundesliga/table");
+            console.log("here maybe?");
+            scrape("https://www.bbc.com/sport/football/german-bundesliga/table");
+        }  
     }
 
     async function scrape(url) {
@@ -49,6 +53,7 @@ async function searchStanding(page, obj) {
 
         if(targetTeam) {
             console.log(`${obj.team} is currently in position ${targetTeam.rank} on ${obj.league}.`);
+            found = true;
         } else {
             console.log(`${obj.team} is not on ${obj.league}.`);
         } // no need to close page; index.js does it already
