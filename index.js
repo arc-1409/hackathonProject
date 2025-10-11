8701b622c7fc4452a692e11ea6a0cac8df41eb40
arc-1409 <lulu.august.liao@gmail.com>
2025-10-11 01:10:56 -0400

    add font link

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ async function main(command, obj) {
     const horizontal = line.repeat(terminalWidth);
 
     console.log(horizontal);
-    console.log(chalk.yellow(figlet.textSync("FootballWatcher", {horizontalLayout:"full", font: "Star Wars"})) );
+    console.log(chalk.yellow(figlet.textSync("FootballWatcher", {horizontalLayout:"full", font: "Star Wars"})) ); // font: https://www.figlet.org/examples.html
     console.log(horizontal);
 
     try {
