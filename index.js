c74e3ce250e7687093ad373853341165b38a7149
arc-1409 <lulu.august.liao@gmail.com>
2025-10-17 20:22:18 -0400

    fix typo

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ program
             process.exit(1);
         } 
 
-        if (!(targetLeauge in leagueList)) {
+        if (!(targetLeague in leagueList)) {
             console.error("ERROR: please specify valid league name or omit.");
             process.exit(1);
         }
